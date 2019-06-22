2ee788e1bbcfa112b5673e78165fb5e3d9eb5849
lingyunsheng <1841046698@qq.com>
2019-06-22 21:25:00 +0800

    reducer

---
+++ react-dynamicfield-redux-count/README.md
@@ -0,0 +1,18 @@
+## 状态管理
+
+redux
+mobx
+
+应用的状态放在全局的位置
+var a=1
+定制了 一套更改数据的流程 难
+
+## action
+js {}
+干什么 type:'increment'
+传递数据
+
+## Reducer
+函数 更新 store
+
+## store 存数据的地方
---
+++ react-dynamicfield-redux-count/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DynamicField from './DynamicField'
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div>
+      <DynamicField />
+    </div>
+  );
+}
+
+export default App;
