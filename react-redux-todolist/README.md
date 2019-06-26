e5415488c459324a36d501ee156d5028aeb25a6d
lingyunsheng <1841046698@qq.com>
2019-06-26 12:03:42 +0800

    redux 数据管理 todo

---
+++ react-redux-todolist/README.md
@@ -0,0 +1,4 @@
+## redux
+ cnpm i redux -S   数据管理
+
+  cnpm i react-redux -S  数据用到组件上
---
+++ react-redux-todolist/src/reducer/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import todos from './todos';
+import filterReducer from './filter';
+export default combineReducers({
+    todos,
+    filterType:filterReducer
+})
