ba4c27c4505a1dae66e5ae92cc359c7bb782489c
lingyunsheng <1841046698@qq.com>
2019-06-27 14:28:33 +0800

    react

--- react-hoc/src/App.js
+++ react-hoc/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LoginStatus from './LoginStatus';
+import ExampleMobx from './exampleMobx/index';
 import ShopCart from './ShopCart';
-// import WithLogin from './WithLogin';
-import './App.css';
 import withLogin from './WithLogin';
+import examplebase from './examplebase/index';
+import './App.css';
+
 
 // const WithLoginStatus = withLogin(LoginStatus);
 // const WithShopCart = withLogin(ShopCart);
@@ -15,6 +17,7 @@ function App() {
       <ShopCart />
       {/* <WithLoginStatus />
       <WithShopCart a="1" b="2"/> */}
+      <ExampleMobx />
     </React.Fragment>
   )
 };
